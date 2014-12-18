8b804f214f6c443a9eb42decf3d507f10e79acb4
Rachelle Keblitis <rkeblitis@gmail.com>
2014-12-18 12:35:36 -0800

    make httpary get requesto to ship-to-me api

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -6,6 +6,18 @@ class CartController < ApplicationController
     end
   end
 
+  def shipping
+    # send an api request to ship-to-me
+    # include the weight, dimenions, cylinder of the package in the cart
+    shipping location params
+    @estimate = HTTPARTY.get("https://localhost:4000/rates", query: {
+      origin: {country: "USA", state: "WA", city: "Seattle", zip: "98103"},
+      destination: {country: "USA", state: "NJ", city: "Bordentown", zip: "08505"
+    }})
+    # @estimate_parsed_response =
+  end
+
+
   def show
   end
 end
